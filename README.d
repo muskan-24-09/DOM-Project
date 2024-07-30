a318ede4722a366c8fc0f9c83da232b5860b7f74
muskan-24-09 <131532183+muskan-24-09@users.noreply.github.com>
2024-07-30 21:46:36 +0530

    Added REAME File

---
+++ README.d
@@ -0,0 +1,25 @@
+**DOM Project-1**
+
+This project demonstrates basic manipulation of the DOM (Document Object Model) using JavaScript. It showcases how to access and modify HTML elements dynamically. Below is a brief description of what the code does:
+
+**Code Description:**
+
+1. HTML Structure:
+
+-> A simple HTML document containing a `div` element with some initial inline styles.
+->The `div` has an ID of `myDiv`, which is used to reference the element in JavaScript.
+
+2. JavaScript:
+
+->The JavaScript code accesses the `div` element using `document.getElementById`.
+It then changes various styles of the `div` element, such as text color, background color, margin, padding, font size, font weight, height, and width.
+
+
+**Key Features:**
+
+-> Dynamic Style Manipulation: Demonstrates how to dynamically change the styles of an HTML element using JavaScript.
+-> DOM Access: Shows how to access an HTML element by its ID.
+-> Inline Styling: The initial styles are set inline within the `div` element for demonstration purposes.
+
+**Usage:**
+To view this project, simply open the HTML file in a web browser. The JavaScript code will execute automatically, applying the specified styles to the `div` element.
